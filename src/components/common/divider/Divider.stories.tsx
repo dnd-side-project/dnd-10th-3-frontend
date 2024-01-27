2f34912fa9fb4f3446b8ae0a74f02dbb0b143fe9
Sung Ji Hyun <69228045+jhsung23@users.noreply.github.com>
2024-01-27 16:50:33 +0900

    [FEAT] Divider 컴포넌트, 스토리북 (#41)

---
+++ src/components/common/divider/Divider.stories.tsx
@@ -0,0 +1,123 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Typography } from '@/components/common/typography';
+
+import Divider from './Divider';
+
+const meta: Meta<typeof Divider> = {
+  title: 'components/common/divider',
+  component: Divider,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Divider>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const DummyTypo = () => (
+  <Typography>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</Typography>
+);
+
+export const Basic: Story = {
+  render: () => (
+    <>
+      <DummyTypo />
+      <Divider />
+      <DummyTypo />
+    </>
+  ),
+};
+
+export const WithWidth: Story = {
+  render: () => (
+    <>
+      <DummyTypo />
+      <Divider width={'25%'} />
+      <DummyTypo />
+      <Divider width={'50%'} />
+      <DummyTypo />
+      <Divider width={'100%'} />
+      <DummyTypo />
+    </>
+  ),
+};
+
+export const WithHeight: Story = {
+  render: () => (
+    <>
+      <DummyTypo />
+      <Divider height={'sm'} />
+      <DummyTypo />
+      <Divider height={'md'} />
+      <DummyTypo />
+      <Divider height={'lg'} />
+      <DummyTypo />
+    </>
+  ),
+};
+
+export const WithMargin: Story = {
+  render: () => (
+    <>
+      <DummyTypo />
+      <Divider margin={'xs'} />
+      <DummyTypo />
+      <Divider margin={'sm'} />
+      <DummyTypo />
+      <Divider margin={'md'} />
+      <DummyTypo />
+      <Divider margin={'lg'} />
+      <DummyTypo />
+      <Divider margin={'xl'} />
+      <DummyTypo />
+    </>
+  ),
+};
+
+export const WithColor: Story = {
+  render: () => (
+    <>
+      <DummyTypo />
+      <Divider color="dimmed" />
+      <DummyTypo />
+      <Divider color="primary" />
+      <DummyTypo />
+    </>
+  ),
+};
+
+export const Playground: Story = {
+  argTypes: {
+    width: {
+      control: { type: 'inline-radio' },
+      options: ['25%', '50%', '100%'],
+    },
+    margin: {
+      control: { type: 'inline-radio' },
+      options: ['xs', 'sm', 'md', 'lg', 'xl'],
+    },
+    color: {
+      control: { type: 'inline-radio' },
+      options: ['primary', 'dimmed'],
+    },
+    height: {
+      control: { type: 'inline-radio' },
+      options: ['sm', 'md', 'lg'],
+    },
+  },
+  args: {
+    width: '100%',
+    margin: 'md',
+    color: 'dimmed',
+    height: 'sm',
+  },
+  render: (args) => (
+    <>
+      <DummyTypo />
+      <Divider {...args} />
+      <DummyTypo />
+    </>
+  ),
+};
