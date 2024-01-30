cbe1a89ca0a812e427663e0bd5acb4f21f5eb92e
CodyMan0 <hys83751952@gmail.com>
2024-01-30 14:38:03 +0900

    :recycle: Foundation 상위 카테고리 적용

--- src/components/foundation/typography/Typography.stories.tsx
+++ src/components/foundation/typography/Typography.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Typography } from './Typography';
 
 const meta: Meta<typeof Typography> = {
-  title: 'components/common/typography',
+  title: 'foundation/Typography',
   component: Typography,
   argTypes: {
     type: {
