7790b061baeaafcab5e2b01cef18ce4f7620dbd8
CodyMan0 <hys83751952@gmail.com>
2024-01-31 15:11:52 +0900

    :recycle: Colors와 Typo 폴더 이동

--- src/components/common/divider/Divider.stories.tsx
+++ src/components/common/divider/Divider.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Typography } from '@/components/foundation/typography';
+import { Typography } from '@/foundations/typography';
 
 import Divider from './Divider';
 
