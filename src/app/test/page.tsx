e7469f66418c9053ac9f4f2994afb3be0bf0dc83
CodyMan0 <hys83751952@gmail.com>
2024-01-31 16:58:47 +0900

    :lipstick: 390px 이상 UI 적용하여 구현

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -7,20 +7,20 @@ import { Typography } from '@/components/common/typography';
 
 const Home = () => {
   return (
-    <div className="bg- relative flex h-[100dvh] w-full flex-col items-center overflow-hidden bg-red-20 px-4xs">
-      <div className="flex w-full items-center justify-between px-3xs pt-2xl">
+    <div className="relative flex h-[100dvh] w-full flex-col items-center overflow-hidden px-4xs">
+      <div className="flex w-full items-center justify-between px-3xs pt-2xl 390:pt-sm">
         <div className="">
           <Image src={LogoImage} width={50} height={20} alt="logo" priority />
         </div>
         <div className="bg-gray-40 h-sm w-6xl"></div>
         <div>아이콘</div>
       </div>
-      <div className="z-10 flex h-[300px] flex-col items-center justify-center gap-[24px] py-xl">
+      <div className="z-10 flex w-full flex-col items-center justify-center gap-[24px] py-xl 390:h-[200px] 390:py-xs">
         <Image src={LogoImage} width={150} height={91} alt="logo" priority />
         <Typography type={'heading2'}>축의금 결정이 너무 어려웠나요?</Typography>
       </div>
 
-      <div className="py-xs">
+      <div className="flex w-[110%] justify-center py-xs  390:py-4xs">
         <Image src={WorryImage} width={275} alt="logo" priority />
       </div>
       <div className="flex w-full justify-center px-5xs py-3xs">
@@ -30,8 +30,8 @@ const Home = () => {
           </Typography>
         </Button>
       </div>
-      <div className="flex h-[80vh] w-full flex-col items-center">
-        <div className="absolute bottom-[10px] flex w-full flex-col gap-3xs px-xs pb-[25px]">
+      <div className="flex w-full flex-col items-center">
+        <div className="absolute bottom-[10px] flex w-full flex-col px-xs">
           <Button className="bg-gray-30" width="full">
             <Typography type={'body1'}>테스트하고 축의금 알아보기</Typography>
           </Button>
