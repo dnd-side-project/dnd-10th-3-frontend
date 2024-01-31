8d7e2c7d17324542aff3c55d0ee83972fcca09f3
CodyMan0 <hys83751952@gmail.com>
2024-01-31 17:25:19 +0900

    :lipstick: Foundation 디자인 시스템와 병합 후 기본 테스트 UI 구현

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,16 +3,16 @@ import Image from 'next/image';
 import LogoImage from '@/assets/images/logo.png';
 import WorryImage from '@/assets/images/test-worry.png';
 import { Button } from '@/components/common/button';
-import { Typography } from '@/components/common/typography';
+import { Typography } from '@/foundations/typography';
 
 const Home = () => {
   return (
-    <div className="relative flex h-[100dvh] w-full flex-col items-center overflow-hidden px-4xs">
+    <div className="relative flex h-[100dvh] w-full flex-col items-center overflow-hidden bg-mainGradient  px-4xs">
       <div className="flex w-full items-center justify-between px-3xs pt-2xl 390:pt-sm">
         <div className="">
           <Image src={LogoImage} width={50} height={20} alt="logo" priority />
         </div>
-        <div className="bg-gray-40 h-sm w-6xl"></div>
+        <div className="h-sm w-6xl bg-gray-400"></div>
         <div>아이콘</div>
       </div>
       <div className="z-10 flex w-full flex-col items-center justify-center gap-[24px] py-xl 390:h-[200px] 390:py-xs">
@@ -24,16 +24,18 @@ const Home = () => {
         <Image src={WorryImage} width={275} alt="logo" priority />
       </div>
       <div className="flex w-full justify-center px-5xs py-3xs">
-        <Button className="bg-gray-20 w-3/4" width="full">
-          <Typography type={'body1'} className="text-gray-40">
+        <Button className="w-3/4 bg-gray-100" width="full">
+          <Typography type={'body1'} className="text-gray-300">
             상대 이름을 입력해주세요
           </Typography>
         </Button>
       </div>
       <div className="flex w-full flex-col items-center">
         <div className="absolute bottom-[10px] flex w-full flex-col px-xs">
-          <Button className="bg-gray-30" width="full">
-            <Typography type={'body1'}>테스트하고 축의금 알아보기</Typography>
+          <Button className="bg-gray-200" width="full">
+            <Typography type={'body1'} className="text-white">
+              테스트하고 축의금 알아보기
+            </Typography>
           </Button>
         </div>
       </div>
