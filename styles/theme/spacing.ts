249e4394fd49f81ec2459a2242cc64d2672ba193
sungjihyun <jhsung23@gmail.com>
2024-02-04 23:43:17 +0900

    :broom: spacing 단위 변경 (px to rem)

--- styles/theme/spacing.ts
+++ styles/theme/spacing.ts
@@ -1,19 +1,19 @@
 export const spacing = {
-  none: '0px',
-  '7xs': '2px',
-  '6xs': '4px',
-  '5xs': '8px',
-  '4xs': '12px',
-  '3xs': '16px',
-  '2xs': '20px',
-  xs: '24px',
-  sm: '32px',
-  md: '40px',
-  lg: '48px',
-  xl: '56px',
-  '2xl': '64px',
-  '3xl': '72px',
-  '4xl': '80px',
-  '5xl': '104px',
-  '6xl': '120px',
+  none: '0rem', // 0px
+  '7xs': '0.125rem', // 2px
+  '6xs': '0.25rem', // 4px
+  '5xs': '0.5rem', // 8px
+  '4xs': '0.75rem', // 12px
+  '3xs': '1rem', // 16px
+  '2xs': '1.25rem', // 20px
+  xs: '1.5rem', // 24px
+  sm: '2rem', // 32px
+  md: '2.5rem', // 40px
+  lg: '3rem', // 48px
+  xl: '3.5rem', // 56px
+  '2xl': '4rem', // 64px
+  '3xl': '4.5rem', // 72px
+  '4xl': '5rem', // 80px
+  '5xl': '6.5rem', // 104px
+  '6xl': '7.5rem', // 120px
 };
