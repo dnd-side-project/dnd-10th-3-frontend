ee03ee4c14f682d792165e0b1c872924aa35edce
sungjihyun <jhsung23@gmail.com>
2024-02-05 00:36:06 +0900

    :lipstick: 로그인 페이지에 헤더 적용 및 헤더 width 수정

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,13 +2,16 @@ import Image from 'next/image';
 
 import worry from '@/assets/images/test-worry.png';
 import { KakaoLoginButton } from '@/components/AuthButton';
+import { Header } from '@/components/layout/header';
 import { Typography } from '@/foundations/typography';
 
 const LoginPage = () => {
   return (
-    <div className="flex min-h-full flex-col items-center bg-mainGradient px-4 pb-10">
-      {/* TODO 헤더 구성 */}
-      <header className="h-24 w-full bg-slate-200">header</header>
+    <div className="relative flex h-dvh w-full flex-col items-center bg-mainGradient px-4 pb-10">
+      <Header>
+        <Header.Logo />
+        <Header.IconLink href="/my-page" icon="user" />
+      </Header>
 
       <div className="mt-16 flex flex-col items-center gap-1">
         <Typography type="body2">3초만에 로그인하고</Typography>
