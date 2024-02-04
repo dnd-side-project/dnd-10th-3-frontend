33e9ed1f15b6a1cc2f9c272202cde1a55a8880b6
sungjihyun <jhsung23@gmail.com>
2024-02-05 00:16:09 +0900

    :lipstick: 카카오 버튼 디자인 완성

--- src/components/AuthButton/KakaoLoginButton.tsx
+++ src/components/AuthButton/KakaoLoginButton.tsx
@@ -5,11 +5,12 @@ import { Button } from '@/components/common/button';
 const KakaoLoginButton = () => {
   return (
     <Button
-      icon="icon" // TODO kakao icon
+      icon="kakaotalk"
       width="full"
-      className="gap-0 bg-auth-kakao px-9 text-gray-650"
+      iconColor="black"
+      className="bg-auth-kakao px-md text-gray-600"
     >
-      카카오로 시작하기
+      <span className="grow">카카오로 시작하기</span>
     </Button>
   );
 };
