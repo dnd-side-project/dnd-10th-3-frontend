f3fb4f6634167b530866b980d580bfc098e4f2c5
Sung Ji Hyun <69228045+jhsung23@users.noreply.github.com>
2024-02-04 21:26:09 +0900

    [FEAT] Button 컴포넌트에 `empty` variant 추가  (#65)

--- src/components/common/button/variant.tsx
+++ src/components/common/button/variant.tsx
@@ -1,13 +1,13 @@
 import { cva } from 'class-variance-authority';
 
-// TODO: 디자인 시안 적용 필요 (현재는 임시 디자인)
 export const buttonVariants = cva(
-  'inline-flex select-none items-center justify-center gap-5xs rounded-md px-3xs py-4xs text-base',
+  'inline-flex select-none items-center justify-center gap-5xs rounded-md px-3xs py-4xs text-base font-medium',
   {
     variants: {
       variant: {
         primary: 'bg-gray-600 text-gray-100',
         secondary: 'bg-gray-100 text-gray-600',
+        empty: 'bg-transparent',
       },
       width: {
         fit: 'w-fit min-w-fit',
