fda7c0b20ceb3f2087aaee3a539f5090b75abd9f
이주영 <93697790+CodyMan0@users.noreply.github.com>
2024-02-04 21:12:19 +0900

    [FEAT] Tag 공통 컴포넌트 작성  (#62)

---
+++ src/components/common/tag/Tag.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Tag } from './Tag';
+
+const meta = {
+  title: 'components/common/tag',
+  component: Tag,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Tag>;
+
+export default meta;
+
+type Story = StoryObj<typeof Tag>;
+
+export const Basic = {
+  render: () => (
+    <div className="flex gap-4xs">
+      <div className="flex flex-col items-center justify-center gap-4xs">
+        <Tag variant="primary">진행중</Tag>
+        <Tag variant="primary">전체</Tag>
+        <Tag variant="primary">참여한 투표</Tag>
+        <Tag variant="primary">만든 투표</Tag>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-4xs">
+        <Tag variant="secondary">마감</Tag>
+        <Tag variant="secondary">전체</Tag>
+        <Tag variant="secondary">참여한 투표</Tag>
+        <Tag variant="secondary">만든 투표</Tag>
+      </div>
+      <div className="flex flex-col items-center justify-center gap-4xs">
+        <Tag variant="primary">1/10</Tag>
+      </div>
+    </div>
+  ),
+} satisfies Story;
