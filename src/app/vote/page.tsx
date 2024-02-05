e785318d2adb31029d5a189fd6151d80e75dbb85
CodyMan0 <hys83751952@gmail.com>
2024-02-05 16:14:19 +0900

    :lipstick: Search 영역 UI 구현

--- src/app/vote/page.tsx
+++ src/app/vote/page.tsx
@@ -1,5 +1,7 @@
+import { Input } from '@/components/common/input';
 import { Header } from '@/components/layout/header';
 import PageLayout from '@/components/layout/PageLayout';
+import { Typography } from '@/foundations/typography';
 
 const VotePage = () => {
   return (
@@ -9,6 +11,17 @@ const VotePage = () => {
         <Header.Tab />
         <Header.IconLink href="/my-page" icon="user" />
       </Header>
+
+      {/* <카테고리 탭/> */}
+      <div className="p-xs">
+        <Typography type={'heading1'}>
+          결혼식 참석시 <br /> 고민이었던 부분을 나눠보세요.
+        </Typography>
+        {/* searchInput을*/}
+        <div className="py-4xs">
+          <Input placeholder="무엇이 고민이신가요?" />
+        </div>
+      </div>
     </PageLayout>
   );
 };
