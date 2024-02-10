05b0668d2685b31ab956e6b92e1fedf44723631f
CodyMan0 <hys83751952@gmail.com>
2024-02-10 12:58:37 +0900

    :recycle: absolute에서 flex로 수직 정렬 리팩토링

--- src/app/vote/page.tsx
+++ src/app/vote/page.tsx
@@ -183,7 +183,7 @@ const VotePage = () => {
           </div>
         </>
       ) : (
-        <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/3">
+        <div className="flex flex-1 items-center justify-center">
           <EmptyVoteLayout />
         </div>
       )}
