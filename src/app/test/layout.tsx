c57b6c14d3dffb5f44683b622cd337a5201668c0
CodyMan0 <hys83751952@gmail.com>
2024-02-11 22:21:58 +0900

    :sparkles: testing 라우트 제거하여 test로 병합

--- src/app/test/layout.tsx
+++ src/app/test/layout.tsx
@@ -5,17 +5,7 @@ import type { PropsWithChildren } from 'react';
 import CreateGoalFormProvider from '@/contexts/reactHookForm/CreateGoalFormProvider';
 
 const HomeLayout = ({ children }: PropsWithChildren) => {
-  return (
-    <CreateGoalFormProvider>
-      <main className="relative flex justify-center">
-        <div className="h-[100dvh] w-full">
-          <div className="flex h-[100dvh] w-full flex-col items-center justify-between pb-xs">
-            {children}
-          </div>
-        </div>
-      </main>
-    </CreateGoalFormProvider>
-  );
+  return <CreateGoalFormProvider>{children}</CreateGoalFormProvider>;
 };
 
 export default HomeLayout;
