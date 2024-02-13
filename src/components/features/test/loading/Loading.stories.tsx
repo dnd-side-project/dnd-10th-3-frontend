f0cd9a54570e48462574984a0edf1c6b5fcf7fd7
CodyMan0 <hys83751952@gmail.com>
2024-02-13 22:15:38 +0900

    :lipstick: Loading 컴포넌트 구현

---
+++ src/components/features/test/loading/Loading.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Loading from './Loading';
+
+const meta = {
+  title: 'components/features/loading',
+  component: Loading,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  decorators: [
+    (Story) => (
+      <div className="bg-slate-200">
+        <div className="layout bg-white p-2xs">
+          <div className="flex w-full gap-2">
+            <Story />
+          </div>
+        </div>
+      </div>
+    ),
+  ],
+  tags: ['autodocs'],
+} satisfies Meta<typeof Loading>;
+
+export default meta;
+type Story = StoryObj<typeof Loading>;
+
+export const Basic: Story = {};
---
+++ src/components/features/test/loading/Loading.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+
+import ErrorWorry from '@/assets/images/error-worry.png';
+import { ProgressBar } from '@/components/common/progressBar';
+import { Typography } from '@/foundations/typography';
+import { Range } from '@/types/util';
+
+type Props = {
+  progress: Range<0, 101>;
+};
+
+const Loading = ({ progress }: Props) => {
+  return (
+    <div className="flex w-full flex-col items-center justify-center gap-xl">
+      <Typography type="heading1">계산하는중</Typography>
+      <Image src={ErrorWorry} width={126} alt="loadingWorry" />
+      <ProgressBar currentProgress={progress} />
+    </div>
+  );
+};
+
+export default Loading;
