2fc8f4e7bfc0c104d05f048554daca18ea1200e5
이주영 <93697790+CodyMan0@users.noreply.github.com>
2024-02-13 14:20:00 +0900

    [DESIGN] 디자인 피드백 반영하여 변경된 디자인 작업  (#79)

--- src/components/common/input/variant.tsx
+++ src/components/common/input/variant.tsx
@@ -1,41 +1,36 @@
 import { cva } from 'class-variance-authority';
 
-export const inputContainerVariants = cva(
-  'flex w-full items-center gap-5xs border p-3xs shadow-thumb',
-  {
-    variants: {
-      variant: {
-        empty: 'border-none shadow-none',
-      },
-      bgcolor: {
-        white: 'bg-white',
-        gray: 'bg-gray-100',
-      },
-      borderRadius: {
-        basic: 'rounded-md',
-        large: 'rounded-lg',
-        larger: 'rounded-3xl',
-      },
-      height: {
-        basic: 'h-md',
-        large: 'h-[52px]',
-        larger: 'h-xl',
-      },
-      isFocused: {
-        true: 'border-primary-700',
-        false: 'border-gray-100',
-      },
-      disabled: {
-        true: 'cursor-not-allowed opacity-50',
-      },
+export const inputContainerVariants = cva('flex w-full items-center gap-5xs border p-3xs ', {
+  variants: {
+    variant: {
+      empty: 'border-none bg-inherit shadow-none',
     },
-    defaultVariants: {
-      borderRadius: 'basic',
-      height: 'basic',
-      bgcolor: 'white',
+    bgcolor: {
+      white: 'bg-white',
+      gray: 'bg-gray-100',
+    },
+    borderRadius: {
+      basic: 'rounded-md',
+      large: 'rounded-lg',
+    },
+    height: {
+      basic: 'h-[36px]',
+      large: 'h-md',
+    },
+    isFocused: {
+      true: 'border-primary-700 bg-white',
+      false: 'border-gray-100',
+    },
+    disabled: {
+      true: 'cursor-not-allowed opacity-50',
     },
   },
-);
+  defaultVariants: {
+    borderRadius: 'basic',
+    height: 'basic',
+    bgcolor: 'gray',
+  },
+});
 
 export const inputVariants = cva(
   ' w-full bg-transparent placeholder:text-gray-300 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 ',
