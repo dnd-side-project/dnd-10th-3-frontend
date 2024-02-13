e8130b831390d182d8803b0e7c5c85b4fe2c0394
sungjihyun <jhsung23@gmail.com>
2024-02-13 13:52:54 +0900

    :lipstick: 라디오 버튼 색깔 변경

--- src/components/common/radio/Radio.variant.tsx
+++ src/components/common/radio/Radio.variant.tsx
@@ -1,12 +1,12 @@
 import { cva } from 'class-variance-authority';
 
 export const radioVariant = cva(
-  'size-4 cursor-pointer appearance-none rounded-full border-[1.5px] border-gray-200 bg-transparent outline-none checked:border-[3px] checked:border-solid checked:border-transparent checked:bg-clip-padding',
+  'size-4 cursor-pointer appearance-none rounded-full border-[1.5px] border-gray-400 bg-transparent outline-none checked:border-[3px] checked:border-solid checked:border-transparent checked:bg-clip-padding',
   {
     variants: {
       color: {
         primary: 'checked:bg-primary-700 checked:shadow-radio-primary',
-        gray: 'checked:bg-gray-300 checked:shadow-radio-gray',
+        gray: 'checked:bg-gray-400 checked:shadow-radio-gray',
       },
     },
     defaultVariants: {
