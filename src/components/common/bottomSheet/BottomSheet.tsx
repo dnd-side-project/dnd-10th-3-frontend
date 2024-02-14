d607b76f3b16160bc371ea8b1cadd2d84e4d6352
sungjihyun <jhsung23@gmail.com>
2024-02-14 12:55:29 +0900

    :construction: 바텀 시트 props

--- src/components/common/bottomSheet/BottomSheet.tsx
+++ src/components/common/bottomSheet/BottomSheet.tsx
@@ -9,7 +9,7 @@ import './style.css';
 export interface BottomSheetProps extends BaseBottomSheetProps {
   HeaderComponent?: React.ReactNode;
   FooterComponent?: React.ReactNode;
-  fixedMaxHeight?: number;
+  // fixedMaxHeight?: number;
 }
 //test: rebase 후 push 할경우 실패 에러 체크
 export const BottomSheet = forwardRef<RefHandles, BottomSheetProps>(
@@ -19,7 +19,7 @@ export const BottomSheet = forwardRef<RefHandles, BottomSheetProps>(
       onDismiss,
       HeaderComponent,
       FooterComponent,
-      fixedMaxHeight,
+      // fixedMaxHeight,
       children,
       ...props
     }: BottomSheetProps,
@@ -31,7 +31,8 @@ export const BottomSheet = forwardRef<RefHandles, BottomSheetProps>(
         ref={ref}
         open={open}
         onDismiss={onDismiss}
-        snapPoints={({ maxHeight }) => fixedMaxHeight || maxHeight - maxHeight / 5}
+        // snapPoints가 적용된 이유가 무엇인가요? 이걸 적용하지 않는다면 바텀시트의 height가 컨텐츠에 맞게 조정되네요?
+        // snapPoints={({ maxHeight }) => fixedMaxHeight || maxHeight - maxHeight / 5}
         expandOnContentDrag
         header={HeaderComponent}
         footer={FooterComponent}
