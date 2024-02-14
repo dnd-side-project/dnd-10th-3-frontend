27c036b42b7f3c88f79160b01695648a9bb425a0
sungjihyun <jhsung23@gmail.com>
2024-02-14 20:56:28 +0900

    :heavy_plus_sign: 토스트 문구

---
+++ src/constants/toast.ts
@@ -0,0 +1,28 @@
+export const TOAST_MESSAGES = Object.freeze({
+  VOTE_UPLOAD_SUCCESS: '투표 업로드가 완료되었습니다',
+  VOTE_UPLOAD_FAIL: '투표 업로드에 실패했습니다',
+
+  VOTE_MODIFY_SUCCESS: '투표 내용 수정이 완료되었습니다',
+  VOTE_MODIFY_FAIL: '투표 내용 수정에 실패했습니다',
+
+  VOTE_DELETE_SUCCESS: '투표 삭제가 완료되었습니다',
+  VOTE_DELETE_FAIL: '투표 삭제에 실패했습니다',
+
+  VOTE_TITLE_REQUIRED: '제목을 입력해 주세요',
+  VOTE_CONTENT_REQUIRED: '내용을 입력해 주세요',
+  LEAST_TWO_ITEMS_REQUIRED: '투표 항목을 2개 이상 입력해 주세요',
+
+  VOTING_SUCCESS: '투표 참여가 완료되었습니다',
+  VOTING_FAIL: '투표 참여에 실패했습니다',
+
+  VOTING_MODIFY_SUCCESS: '투표 응답 수정이 완료되었습니다',
+  VOTING_MODIFY_FAIL: '투표 응답 수정에 실패했습니다',
+
+  REPLY_REGISTER_SUCCESS: '댓글 등록이 완료되었습니다',
+  REPLY_REGISTER_FAIL: '댓글 등록에 실패했습니다',
+
+  REPLY_DELETE_SUCCESS: '댓글 삭제가 완료되었습니다',
+  REPLY_DELETE_FAIL: '댓글 삭제에 실패했습니다',
+
+  LINK_COPIED: '링크가 복사되었습니다',
+});
