e01759da2753ddcccbf664ec1ba8d06a57f6f321
sungjihyun <jhsung23@gmail.com>
2024-02-14 12:55:08 +0900

    :sparkles: 카테고리 선택 기능

---
+++ src/app/vote/create/_components/CategorySelector.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { useState } from 'react';
+
+import { BottomSheet } from '@/components/common/bottomSheet';
+import { Icon } from '@/components/common/icon';
+import { OptionButton } from '@/components/shared';
+import { CATEGORIES } from '@/constants/category';
+import { Typography } from '@/foundations/typography';
+
+type SelectedCategory = undefined | (typeof CATEGORIES)[number];
+
+const CategorySelector = () => {
+  const [selectedCategory, setSelectedCategory] = useState<SelectedCategory>(undefined);
+  const [isBottomSheetOpen, setIsBottomSheetOpen] = useState(false);
+
+  const openBottomSheet = () => setIsBottomSheetOpen(true);
+  const closeBottomSheet = () => setIsBottomSheetOpen(false);
+
+  return (
+    <>
+      <button
+        onClick={openBottomSheet}
+        className="flex items-center justify-between px-2xs py-3xs text-gray-1000"
+      >
+        {selectedCategory ?? '투표의 주제를 선택해 주세요.'}
+        <Icon icon="chevronRight" color="gray-1000" size={14} />
+      </button>
+      <BottomSheet open={isBottomSheetOpen} onDismiss={closeBottomSheet}>
+        <Typography type="heading3" className="p-3xs text-gray-1000">
+          투표의 주제를 선택해 주세요.
+        </Typography>
+        {CATEGORIES.map((category) => (
+          <OptionButton
+            key={category}
+            option={category}
+            isChecked={category === selectedCategory}
+            onClick={() => {
+              setSelectedCategory(category);
+              setIsBottomSheetOpen(false);
+            }}
+          />
+        ))}
+      </BottomSheet>
+    </>
+  );
+};
+
+export default CategorySelector;
--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,1 +1,2 @@
 export { default as LetterCounter } from './letterCounter/LetterCounter';
+export { default as OptionButton } from './optionButton/OptionButton';
