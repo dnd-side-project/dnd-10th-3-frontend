c7a681a55dc1c14164b3859baf8f02af8b85f422
sungjihyun <jhsung23@gmail.com>
2024-02-14 12:56:53 +0900

    :sparkles: vote/create 페이지

---
+++ src/app/vote/create/page.tsx
@@ -0,0 +1,11 @@
+import { CreateVoteForm } from './_components';
+
+const page = () => {
+  return (
+    <div className="flex size-full min-h-dvh flex-col">
+      <CreateVoteForm />
+    </div>
+  );
+};
+
+export default page;
