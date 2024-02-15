2314aeb7482952e2f42ee791751238704efb1355
CodyMan0 <hys83751952@gmail.com>
2024-02-16 00:11:58 +0900

    :lipstick: Tag 디자인 시스템 컴포넌트 UI 수정

--- src/components/common/tag/variant.tsx
+++ src/components/common/tag/variant.tsx
@@ -1,16 +1,12 @@
 import { cva } from 'class-variance-authority';
 
-export const tagVariants = cva(
-  'flex w-fit cursor-pointer items-center justify-center rounded-xl px-3xs py-6xs',
-  {
-    variants: {
-      variant: {
-        primary: 'bg-primary-200 text-primary-700',
-        secondary: 'bg-gray-100 text-gray-300',
-      },
-    },
-    defaultVariants: {
-      variant: 'primary',
+export const tagVariants = cva('flex w-fit select-none items-center justify-center rounded-xl', {
+  variants: {
+    variant: {
+      primary: 'text-secondary-deep',
     },
   },
-);
+  defaultVariants: {
+    variant: 'primary',
+  },
+});
