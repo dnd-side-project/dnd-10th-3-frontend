1fa7577443698b4f83ea3f8dcf766e1f849ee96e
CodyMan0 <hys83751952@gmail.com>
2024-02-16 02:01:08 +0900

    :sparkles: Avatar 컴포넌트 className으로 UI 스타일링 가능하도록 변경

--- src/components/common/avatar/Avatar.tsx
+++ src/components/common/avatar/Avatar.tsx
@@ -1,19 +1,25 @@
 import Image from 'next/image';
 
+import { cn } from '@/lib/core';
+
 import { Icon } from '../icon';
 import { IconColor } from '../icon/assets';
 
-interface AvatarProps {
+type AvatarProps = {
   size?: number;
   profileImage?: string;
   color?: IconColor;
-}
+  className?: string;
+};
 
-export const Avatar = ({ size = 24, profileImage, color = 'gray-300' }: AvatarProps) => {
+export const Avatar = ({ size = 24, profileImage, color = 'gray-300', className }: AvatarProps) => {
   return (
     <div
       style={{ width: size, height: size }}
-      className="flex cursor-pointer items-center justify-center overflow-hidden rounded-xl"
+      className={cn(
+        'flex cursor-pointer items-center justify-center overflow-hidden rounded-xl',
+        className,
+      )}
     >
       {!!profileImage ? (
         <Image src={profileImage} width={size} height={size} alt="profile_image" />
