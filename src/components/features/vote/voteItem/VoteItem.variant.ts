a7bdf15300f4d790b42e31a3fa90fd06c5910f80
sungjihyun <jhsung23@gmail.com>
2024-02-16 22:22:57 +0900

    :lipstick: VoteItem에서 상하 패딩 제거

--- src/components/features/vote/voteItem/VoteItem.variant.ts
+++ src/components/features/vote/voteItem/VoteItem.variant.ts
@@ -1,7 +1,7 @@
 import { cva } from 'class-variance-authority';
 
 export const voteItemVariant = cva(
-  `relative z-0 flex w-full select-none items-center gap-5xs overflow-hidden rounded-lg border p-3xs text-sm text-gray-700
+  `relative z-0 flex w-full select-none items-center gap-5xs overflow-hidden rounded-lg border px-3xs text-sm text-gray-700
   has-[:checked]:border-primary-700 has-[input[type=text]:focus]:border-gray-700`,
   {
     variants: {
