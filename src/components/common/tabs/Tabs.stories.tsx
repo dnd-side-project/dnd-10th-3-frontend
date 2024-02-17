4e2c812491bca76d7477bbb73f2ccb589658701a
sungjihyun <jhsung23@gmail.com>
2024-02-17 23:56:43 +0900

    :recycle: Tabs를 마이페이지에서도 사용 가능하도록 리팩토링
    -Tab 삭제
    -스토리북 변경
    -TabItem 상수화
    -Tab 클릭 시 params 연결

--- src/components/common/tabs/Tabs.stories.tsx
+++ src/components/common/tabs/Tabs.stories.tsx
@@ -1,9 +1,13 @@
 import { Meta, StoryObj } from '@storybook/react';
+import { useState } from 'react';
+
+import { CATEGORY_TAB, MYPAGE_TAB } from '@/constants/category';
 
 import { Tabs } from '.';
+import { Tab } from './Tabs';
 
 const meta = {
-  title: 'Components/common/Tabs',
+  title: 'Components/common/tabs',
   component: Tabs,
 } satisfies Meta<typeof Tabs>;
 
@@ -11,8 +15,41 @@ export default meta;
 
 type Story = StoryObj<typeof Tabs>;
 
-export const Basic: Story = {
-  render: () => {
-    return <Tabs tabItems={['전체 ', '축의금', '하객룩', '브라이덜 샤워', '기타']} />;
-  },
+// FIXME: Tabs 내부 useSearchParams 호출로 인해 스토리북에서 Tabs를 사용할 수 없어 Tab만을 활용해 임시 구현
+const CategoryTabExample = () => {
+  const [selectedTab, setSelectedTab] = useState('all');
+
+  return (
+    <nav className="no-scrollbar flex shrink-0 gap-3xs overflow-x-scroll scroll-smooth border-b border-gray-100 px-2xs">
+      {CATEGORY_TAB.map((item) => (
+        <button key={item.name} onClick={() => setSelectedTab(item.params)} className="flex">
+          <Tab item={item} isSelected={selectedTab === item.params} />
+        </button>
+      ))}
+    </nav>
+  );
+};
+
+export const CategoryTabs: Story = {
+  render: () => <CategoryTabExample />,
+};
+
+const MypageTabExample = () => {
+  const [selectedTab, setSelectedTab] = useState('vote');
+
+  return (
+    <nav className="no-scrollbar flex shrink-0 gap-3xs overflow-x-scroll scroll-smooth border-b border-gray-100 px-2xs">
+      {MYPAGE_TAB.map((item) => (
+        <>
+          <button key={item.name} onClick={() => setSelectedTab(item.params)} className="flex">
+            <Tab key={item.name} item={item} isSelected={selectedTab === item.params} />
+          </button>
+        </>
+      ))}
+    </nav>
+  );
+};
+
+export const MypageTabs: Story = {
+  render: () => <MypageTabExample />,
 };
