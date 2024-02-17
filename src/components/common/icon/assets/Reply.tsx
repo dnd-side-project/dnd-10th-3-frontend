30dc5870a4e7a5d3bff8c3a6965a195e78bdc213
CodyMan0 <hys83751952@gmail.com>
2024-02-17 11:18:43 +0900

    :bug: SVG 프로퍼티 케멜 케이스로 수정

--- src/components/common/icon/assets/Reply.tsx
+++ src/components/common/icon/assets/Reply.tsx
@@ -10,10 +10,10 @@ const Reply = ({ width, height, color }: React.SVGProps<SVGSVGElement>) => {
     >
       <path
         d="M6 8.07519V8M10 8.07519V8M14 8.07519V8M9.73913 13.8261L5.56522 18V13.8261H4C2.89543 13.8261 2 12.9307 2 11.8261V4C2 2.89543 2.89543 2 4 2H16C17.1046 2 18 2.89543 18 4V11.8261C18 12.9307 17.1046 13.8261 16 13.8261H9.73913Z"
-        stroke={color}
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
