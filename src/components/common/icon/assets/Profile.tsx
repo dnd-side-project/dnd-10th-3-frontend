339916323e2df95189ddd3d0c518a243d5f0a971
sungjihyun <jhsung23@gmail.com>
2024-02-17 18:55:48 +0900

    :broom: profile svg 어트리뷰트 카멜 케이스로 변경

--- src/components/common/icon/assets/Profile.tsx
+++ src/components/common/icon/assets/Profile.tsx
@@ -7,8 +7,9 @@ const Profile = ({ width, height, color }: React.SVGProps<SVGSVGElement>) => {
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 32 32"
       color={color}
+      className="shrink-0"
     >
-      <g clip-path="url(#a)">
+      <g clipPath="url(#a)">
         <rect width="32" height="32" rx="16" fill="currentColor" />
         <path
           d="M22.533 14.534a6.534 6.534 0 1 1-13.068 0 6.534 6.534 0 0 1 13.068 0Zm-1.491 6.702A8.34 8.34 0 0 1 16 22.934c-1.895 0-3.64-.64-5.047-1.7C7.153 22.907 4.8 28.118 4.8 30.4L16 32.5l11.2-2.1c0-2.261-2.427-7.472-6.158-9.164Z"
