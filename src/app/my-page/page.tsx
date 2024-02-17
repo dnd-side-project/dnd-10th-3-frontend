ea1c8df858687ef0ec73a4239d9d49ec1ded0007
sungjihyun <jhsung23@gmail.com>
2024-02-17 23:25:08 +0900

    :broom: my-page -> mypage (경로에서 dash 제거)

--- src/app/my-page/page.tsx
+++
@@ -1,5 +0,0 @@
-const page = () => {
-  return <></>;
-};
-
-export default page;
--- src/app/mypage/info/page.tsx
+++ src/app/mypage/info/page.tsx
@@ -8,7 +8,7 @@ const page = () => {
       <Header className="bg-white">
         <Header.Previous />
         <Header.Text text="내 정보" />
-        <Header.IconLink icon="settings" href="/my-page/settings" iconColor="gray-1000" />
+        <Header.IconLink icon="settings" href="/mypage/settings" iconColor="gray-1000" />
       </Header>
 
       <main className="flex size-full flex-col px-2xs">
