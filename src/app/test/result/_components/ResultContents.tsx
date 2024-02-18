0269dd7a838f6a1467fedd7f17996457793c6ae7
CodyMan0 <hys83751952@gmail.com>
2024-02-19 07:58:08 +0900

    :recycle: 이미지 눌러 저장하기 버튼에서 Typo로 변경

--- src/app/test/result/_components/ResultContents.tsx
+++ src/app/test/result/_components/ResultContents.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 
 import ResultType from '@/assets/images/resultType.png';
 import { Button } from '@/components/common/button';
+import { Icon } from '@/components/common/icon';
 import { ProgressBar } from '@/components/common/progressBar';
 import { VoteCard, VoteItem } from '@/components/features/vote';
 import { Header } from '@/components/layout/header';
@@ -17,15 +18,13 @@ const ResultContents = () => {
         <section className="flex  flex-col items-center justify-center">
           <article className="flex  flex-col items-center justify-center px-2xs ">
             <Image src={ResultType} width={335} height={487} alt="result-image" />
-            <Button
-              variant="empty"
-              className="my-6xs text-gray-500"
-              icon="caretUp"
-              iconSize={12}
-              iconSide="both"
-            >
-              이미지 꾹 - 눌러서 저장하기
-            </Button>
+            <div className="flex items-center gap-5xs py-3xs">
+              <Icon icon="caretUp" color="gray-300" size={12} />
+              <Typography type="body2" className="text-gray-500">
+                이미지 꾹 - 눌러서 저장하기
+              </Typography>
+              <Icon icon="caretUp" color="gray-300" size={12} />
+            </div>
           </article>
 
           <article className="my-4xs flex w-full px-2xs ">
