bf96020fbb4fa6a414bb1ff8caefe8a92363d640
Sung Ji Hyun <69228045+jhsung23@users.noreply.github.com>
2024-02-18 12:40:24 +0900

    [DESIGN] test 시작 페이지 레이아웃, 디자인 수정 (#115)

--- src/app/test/_components/TestForm.tsx
+++ src/app/test/_components/TestForm.tsx
@@ -32,42 +32,36 @@ const TestForm = () => {
   return (
     <>
       {step === QUESTIONS_ORDERS.home && (
-        <main className={'relative flex h-dvh w-full flex-col items-center bg-mainGradient '}>
-          {/* TODO : 아이폰 크기일 경우 INPUT이 화면에서 짤리는 상황, 추후 디자인 수정 이후 변경 필수 */}
+        <div className="relative flex size-full h-dvh flex-col items-center bg-mainGradient pb-2xs">
           <Header>
             <Header.Logo />
             <Header.Tab />
             <Header.IconLink href="/mypage" icon="mypage" />
           </Header>
-          <div className="z-10 flex w-full flex-col items-center justify-center gap-[24px] py-xl 390:h-[200px] 390:py-xs">
-            <Image src={LogoImage} width={150} height={91} alt="logo" priority />
-            <Typography type={'heading2'} className="text-gray-500">
-              축의금 결정이 너무 어려웠나요?
-            </Typography>
-          </div>
 
-          <div className="flex w-[110%] justify-center py-xs  390:py-4xs">
-            <Image src={WorryImage} width={275} alt="logo" priority />
-          </div>
-          <div className="flex w-[80%] justify-center px-5xs py-3xs">
-            <Input
-              type="fit"
-              placeholder=" 상대 이름을 입력해주세요"
-              className="text-center"
-              value={state.buddy}
-              onChange={(e) => dispatch({ type: 'setBuddyName', value: e.target.value })}
-            />
-          </div>
-          <div className="flex w-full flex-col items-center">
-            <div className="absolute bottom-[20px] flex w-[95%] flex-col px-xs">
-              <Button className=" bg-gray-700" width="full">
-                <Typography type={'body1'} className="text-white" onClick={() => setStep(1)}>
-                  테스트하고 축의금 알아보기
-                </Typography>
-              </Button>
+          <main className="flex size-full flex-col items-center px-2xs">
+            <div className="flex h-full flex-col items-center justify-evenly">
+              <div className="flex flex-col items-center gap-4xs">
+                <Image src={LogoImage} width={150} height={91} alt="logo" priority />
+                <Typography type="title1">축의금 결정이 너무 어려웠나요?</Typography>
+              </div>
+
+              <div className="flex flex-col items-center gap-xs">
+                <Image src={WorryImage} width={275} alt="logo" priority />
+                <Input
+                  type="fit"
+                  placeholder=" 상대 이름을 입력해주세요"
+                  className="text-center"
+                  value={state.buddy}
+                  onChange={(e) => dispatch({ type: 'setBuddyName', value: e.target.value })}
+                />
+              </div>
             </div>
-          </div>
-        </main>
+            <Button width="full" onClick={() => setStep(1)}>
+              테스트하고 축의금 알아보기
+            </Button>
+          </main>
+        </div>
       )}
 
       {QUESTIONS.map((question, index) => {
