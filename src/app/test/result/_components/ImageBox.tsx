61e6922aa951a338a1316824349864c5790992c3
CodyMan0 <hys83751952@gmail.com>
2024-02-19 17:34:46 +0900

    :white_check_mark: 서버 이미지 이전 Mock Image를 위한 도메인 설정

--- src/app/test/result/_components/ImageBox.tsx
+++ src/app/test/result/_components/ImageBox.tsx
@@ -1,13 +1,17 @@
 import Image from 'next/image';
 
-import ResultType from '@/assets/images/resultType.png';
 import { Icon } from '@/components/common/icon';
 import { Typography } from '@/foundations/typography';
 
 const ImageBox = () => {
   return (
     <article className="flex flex-col items-center justify-center px-2xs ">
-      <Image src={ResultType} width={335} height={487} alt="result-image" />
+      <Image
+        src="https://velog.velcdn.com/images/sharphand1/post/18fa9fdc-5078-4477-82ed-0a4f9f5587a6/image.png"
+        width={335}
+        height={487}
+        alt="result-image"
+      />
       <div className="flex items-center gap-5xs py-3xs">
         <Icon icon="caretUp" color="gray-300" size={12} />
         <Typography type="body2" className="text-gray-500">
