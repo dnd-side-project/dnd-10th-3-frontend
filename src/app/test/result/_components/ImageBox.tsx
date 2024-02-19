04d98f6831794d724704a7ee12e98e3d4b02ebfb
CodyMan0 <hys83751952@gmail.com>
2024-02-19 19:39:26 +0900

    :sparkles: canva 엘리먼트의 dataURL을 활용하는 라이브러리를 사용하여 이미지 저장 기능 구현

--- src/app/test/result/_components/ImageBox.tsx
+++ src/app/test/result/_components/ImageBox.tsx
@@ -1,17 +1,29 @@
+'use client';
+
 import Image from 'next/image';
+import { useRef } from 'react';
 
 import { Icon } from '@/components/common/icon';
 import { Typography } from '@/foundations/typography';
+import { useDownloadImage } from '@/hooks/useDownloadImage';
 
 const ImageBox = () => {
+  const imageRef = useRef<HTMLButtonElement>(null);
+  const { onDownloadImage } = useDownloadImage({ imageRef });
+
+  const handleDownloadImage = async () => {
+    await onDownloadImage();
+  };
   return (
-    <article className="flex flex-col items-center justify-center px-2xs ">
-      <Image
-        src="https://velog.velcdn.com/images/sharphand1/post/18fa9fdc-5078-4477-82ed-0a4f9f5587a6/image.png"
-        width={335}
-        height={487}
-        alt="result-image"
-      />
+    <article className="flex flex-col items-center justify-center px-2xs">
+      <button onClick={handleDownloadImage} ref={imageRef}>
+        <Image
+          src="https://velog.velcdn.com/images/sharphand1/post/18fa9fdc-5078-4477-82ed-0a4f9f5587a6/image.png"
+          width={335}
+          height={487}
+          alt="result-image"
+        />
+      </button>
       <div className="flex items-center gap-5xs py-3xs">
         <Icon icon="caretUp" color="gray-300" size={12} />
         <Typography type="body2" className="text-gray-500">
