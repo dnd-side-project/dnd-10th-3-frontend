49451db673568293f52aaff19a081fe18c968a93
Sung Ji Hyun <69228045+jhsung23@users.noreply.github.com>
2024-02-19 16:36:53 +0900

    [BUGFIX] useSearchParams 관련 배포 이슈 해결 (#128)

--- src/components/common/tabs/Tabs.stories.tsx
+++ src/components/common/tabs/Tabs.stories.tsx
@@ -1,11 +1,9 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { useState } from 'react';
 
 import { CATEGORY_TAB } from '@/constants/category';
 import { MYPAGE_TAB } from '@/constants/mypage';
 
 import { Tabs } from '.';
-import { Tab } from './Tabs';
 
 const meta = {
   title: 'Components/common/tabs',
@@ -16,41 +14,28 @@ export default meta;
 
 type Story = StoryObj<typeof Tabs>;
 
-// FIXME: Tabs 내부 useSearchParams 호출로 인해 스토리북에서 Tabs를 사용할 수 없어 Tab만을 활용해 임시 구현
-const CategoryTabExample = () => {
-  const [selectedTab, setSelectedTab] = useState('all');
-
-  return (
-    <nav className="no-scrollbar flex shrink-0 gap-3xs overflow-x-scroll scroll-smooth border-b border-gray-100 px-2xs">
-      {CATEGORY_TAB.map((item) => (
-        <button key={item.name} onClick={() => setSelectedTab(item.params)} className="flex">
-          <Tab item={item} isSelected={selectedTab === item.params} />
-        </button>
-      ))}
-    </nav>
-  );
-};
-
 export const CategoryTabs: Story = {
-  render: () => <CategoryTabExample />,
-};
-
-const MypageTabExample = () => {
-  const [selectedTab, setSelectedTab] = useState('vote');
-
-  return (
-    <nav className="no-scrollbar flex shrink-0 gap-3xs overflow-x-scroll scroll-smooth border-b border-gray-100 px-2xs">
-      {MYPAGE_TAB.map((item) => (
-        <>
-          <button key={item.name} onClick={() => setSelectedTab(item.params)} className="flex">
-            <Tab key={item.name} item={item} isSelected={selectedTab === item.params} />
-          </button>
-        </>
-      ))}
-    </nav>
-  );
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+      pathname: '/',
+      query: {
+        tab: ['all', 'money', 'clothes', 'bridal-shower', 'other'],
+      },
+    },
+  },
+  render: () => <Tabs tabItems={CATEGORY_TAB} />,
 };
 
 export const MypageTabs: Story = {
-  render: () => <MypageTabExample />,
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+      pathname: '/',
+      query: {
+        tab: ['test', 'vote'],
+      },
+    },
+  },
+  render: () => <Tabs tabItems={MYPAGE_TAB} />,
 };
