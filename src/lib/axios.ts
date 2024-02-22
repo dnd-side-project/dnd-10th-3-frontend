45f4b0eca5e656c8fe8cd037575f7c65573875a4
CodyMan0 <hys83751952@gmail.com>
2024-02-22 13:29:44 +0900

    :bug: axiosResponse 에러 lib 파일에 추가

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { type AxiosResponse } from 'axios';
 
 import { env } from './env';
 
@@ -38,22 +38,23 @@ instance.interceptors.response.use(
   },
 );
 
+
 export const get = <T>(...args: Parameters<typeof instance.get>) => {
-  return instance.get<T, T>(...args);
+  return instance.get<T, AxiosResponse<T>>(...args)
 };
 
 export const post = <T>(...args: Parameters<typeof instance.post>) => {
-  return instance.post<T, T>(...args);
+  return instance.post<T, AxiosResponse<T>>(...args);
 };
 
 export const put = <T>(...args: Parameters<typeof instance.put>) => {
-  return instance.put<T, T>(...args);
+  return instance.put<T, AxiosResponse<T>>(...args);
 };
 
 export const patch = <T>(...args: Parameters<typeof instance.patch>) => {
-  return instance.patch<T, T>(...args);
+  return instance.patch<T, AxiosResponse<T>>(...args);
 };
 
 export const del = <T>(...args: Parameters<typeof instance.delete>) => {
-  return instance.delete<T, T>(...args);
+  return instance.delete<T, AxiosResponse<T>>(...args);
 };
