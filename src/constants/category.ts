720665dd37db8203dd729077511cecdf72179e8c
CodyMan0 <hys83751952@gmail.com>
2024-02-22 21:36:04 +0900

    :bug: 브라이덜 샤워 스페이스 제외하여 백엔드 데이터와 통일

--- src/constants/category.ts
+++ src/constants/category.ts
@@ -1,13 +1,13 @@
 import { TabItem } from '@/components/common/tabs/Tabs';
 
-export const CATEGORIES = ['축의금', '하객룩', '브라이덜 샤워', '기타'] as const;
+export const CATEGORIES = ['축의금', '하객룩', '브라이덜샤워', '기타'] as const;
 export const CATEGORIES_FOR_TAB = ['전체', ...CATEGORIES] as const;
 
 export const CATEGORY_TAB: TabItem<(typeof CATEGORIES_FOR_TAB)[number]>[] = [
   { name: '전체', params: 'all' },
   { name: '축의금', params: 'money' },
   { name: '하객룩', params: 'clothes' },
-  { name: '브라이덜 샤워', params: 'bridal-shower' },
+  { name: '브라이덜샤워', params: 'bridal-shower' },
   { name: '기타', params: 'other' },
 ];
 
