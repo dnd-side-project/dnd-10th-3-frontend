c96e76d8b60a43472147c27f68033a631eac1ec7
CodyMan0 <hys83751952@gmail.com>
2024-02-23 12:51:34 +0900

    :sparkles: 로컬 스토리지 진행 결과 id값 저장 로직 구현

--- src/hooks/api/test/useCreateTestResult.tsx
+++ src/hooks/api/test/useCreateTestResult.tsx
@@ -1,14 +1,17 @@
 import { useMutation } from '@tanstack/react-query';
 
 import { TEST } from '@/api/test/test';
+import useLocalStorage from '@/hooks/useLocalStorage';
 import { TestFormType } from '@/types/test';
 
 export const useCreateTestResult = () => {
+  const [, saveResultId] = useLocalStorage<number | null>('resultId', null);
   return useMutation({
     mutationFn: (data: TestFormType) => TEST.POST_RESULT(data),
     //TODO : 성공 및 에러 처리
     onSuccess: (response) => {
       console.log('Post 성공, id값', response.data.id);
+      saveResultId(response.data.id);
     },
     onError: (error) => {
       console.log('Post 실패', error);
