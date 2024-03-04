b8709d0d58c1d2a3fc656b35649f0d52e1b8d877
CodyMan0 <hys83751952@gmail.com>
2024-03-04 13:29:28 +0900

    :sparkles: lodash.debounce를 활용하여 사용자 인풋 입력 후 0.5초후 이벤트 발생시키는 함수 구현

--- src/app/vote/_component/VoteContents.tsx
+++ src/app/vote/_component/VoteContents.tsx
@@ -1,8 +1,9 @@
 'use client';
 
+import debounce from 'lodash.debounce';
 import Link from 'next/link';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 import { Button } from '@/components/common/button';
 import { Spinner } from '@/components/common/spinner';
@@ -27,16 +28,28 @@ const VoteContents = () => {
   );
 
   // 의도 : 입력할 경우 바로 결과 SearchResults 컴포넌트에 반영되도록 하기 위해 state를 사용 추후에 검색시 쓰로틀링 혹은 디바운스 적용
-  const onChangeInputHandler = (targetValue: string) => {
+  const onChangeInputValue = (targetValue: string) => {
     setSearchValueState(targetValue);
+    onDebounceHandlerInputValue(targetValue);
   };
 
-  //Enter키가 눌리면 전역 url에 검색 키워드를 추가하는 로직
+  //FIXME : eslint에서 수정
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const onDebounceHandlerInputValue = useCallback(
+    debounce(async (params: string) => {
+      console.log('디바운스 함수 안', params);
+      router.push(`?q=${params}`);
+    }, 500),
+    [],
+  );
+
   const onKeyUpHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       if (!seacrValueState) {
         return router.push('/vote');
       }
+      //의도 : 중복 엔터 방지 queryString과 비교하여 동일하면 EnterKey 방지
+      if (searchQueryValue === seacrValueState) return;
       router.push(`?q=${seacrValueState}`);
     }
   };
@@ -48,7 +61,7 @@ const VoteContents = () => {
         <>
           <div className="flex w-full flex-col">
             <SearchInput
-              onChangeInputHandler={onChangeInputHandler}
+              onChangeInputHandler={onChangeInputValue}
               searchValue={seacrValueState}
               onKeyUpHandler={onKeyUpHandler}
             />
