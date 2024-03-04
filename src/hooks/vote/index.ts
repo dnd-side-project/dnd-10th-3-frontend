64cfa70e536e1da70bac68fb417b32c9b7082f9b
sungjihyun <jhsung23@gmail.com>
2024-03-05 02:01:14 +0900

    :construction: 댓글 수정

--- src/hooks/vote/index.ts
+++ src/hooks/vote/index.ts
@@ -5,4 +5,5 @@ export { useGetVoteById } from './useGetVoteById';
 export { default as useGetVotePaginatedReplies } from './useGetVotePaginatedReplies';
 export { default as useGetVoteReplies } from './useGetVoteReplies';
 export { default as useLikeVoteMutation } from './useLikeVoteMutation';
+export { default as useUpdateVoteReplyMutation } from './useUpdateVoteReplyMutation';
 export { default as useVotingMutation } from './useVotingMutation';
