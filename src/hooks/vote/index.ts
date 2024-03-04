e3d9f2be51fd956d39f02ce0d1ea50b5d3f9d4e8
sungjihyun <jhsung23@gmail.com>
2024-03-04 17:04:01 +0900

    :sparkles: 전체 댓글 쿼리
    페이지네이션이 포함된 기존 useGetVoteReplies는 useGetVotePaginatedReplies로 변경됨

--- src/hooks/vote/index.ts
+++ src/hooks/vote/index.ts
@@ -1,5 +1,6 @@
 export { useGetAllVotes } from './useGetAllVotes';
 export { useGetVoteById } from './useGetVoteById';
+export { default as useGetVotePaginatedReplies } from './useGetVotePaginatedReplies';
 export { default as useGetVoteReplies } from './useGetVoteReplies';
 export { default as useLikeVoteMutation } from './useLikeVoteMutation';
 export { default as useVoteReplyMutation } from './useVoteReplyMutation';
