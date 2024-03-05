ed5d8d1a37aafffec842031c37ec64d7401fb411
CodyMan0 <hys83751952@gmail.com>
2024-03-05 16:08:59 +0900

    :bug: 변수명 변경하여 의도치 않은 에러 해결

--- src/app/vote/_component/search/SearchResults.tsx
+++ src/app/vote/_component/search/SearchResults.tsx
@@ -6,11 +6,11 @@ import { cn } from '@/lib/core';
 import { searchResultVariants } from './variant';
 
 const SearchResults = ({ searchQueryStringValue }: { searchQueryStringValue: string }) => {
-  const isSearchValue = !!searchQueryStringValue;
+  const isSearchTyped = !!searchQueryStringValue;
 
   return (
     <div className="flex w-full justify-between p-6xs px-3xs">
-      <Typography type="title1" className={cn(searchResultVariants({ isSearchValue }))}>
+      <Typography type="title1" className={cn(searchResultVariants({ isSearchTyped }))}>
         <span className="text-[#5382FF]">{searchQueryStringValue}</span> 검색 결과
       </Typography>
 
