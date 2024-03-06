bd629f7cd34095ca8264a621bc2604d67bd57ad4
sungjihyun <jhsung23@gmail.com>
2024-03-06 16:16:22 +0900

    :recycle: LikeButton props 이름 변경, 투표 공감 optimistic update시 immer 사용

--- src/components/shared/likeButton/LikeButton.tsx
+++ src/components/shared/likeButton/LikeButton.tsx
@@ -3,17 +3,17 @@ import { Button } from '@/components/common/button';
 type Props = {
   isLiked: boolean;
   likeCount: number;
-  clickHandler: () => void;
+  onClick: () => void;
 };
 
-const LikeButton = ({ isLiked, likeCount, clickHandler }: Props) => {
+const LikeButton = ({ isLiked, likeCount, onClick }: Props) => {
   return (
     <Button
       icon={isLiked ? 'filledHeart' : 'heart'}
       iconColor="primary-700"
       variant="empty"
       className="gap-6xs !p-0 text-[14px] text-gray-600"
-      onClick={clickHandler}
+      onClick={onClick}
     >
       {likeCount}
     </Button>
