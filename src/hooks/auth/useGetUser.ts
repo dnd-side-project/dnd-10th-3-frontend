9d70e0a22c624db4c93a502ee536c58f06325a5d
sungjihyun <jhsung23@gmail.com>
2024-03-08 16:52:16 +0900

    :sparkles: useGetUser

---
+++ src/hooks/auth/useGetUser.ts
@@ -0,0 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+import { isAxiosError } from 'axios';
+import { hasCookie } from 'cookies-next';
+
+import { IS_LOGIN } from '@/constants/auth';
+import { get } from '@/lib/axios';
+import { SuccessResponse } from '@/types/response';
+import { UserInfo } from '@/types/user';
+
+const getUser = async () => {
+  const response = await get<SuccessResponse<UserInfo>>('/user');
+  return response.data.data;
+};
+
+const useGetUser = () => {
+  return useQuery({
+    queryKey: ['user'],
+    queryFn: getUser,
+    enabled: hasCookie(IS_LOGIN),
+    throwOnError: (error) => isAxiosError(error),
+    staleTime: Infinity,
+    gcTime: Infinity,
+  });
+};
+
+export default useGetUser;
