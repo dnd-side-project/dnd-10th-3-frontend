9a14cb1ae3ab16d5722696000c51737008846a4a
CodyMan0 <hys83751952@gmail.com>
2024-03-09 16:28:46 +0900

    :recycle: 작은 버전의 결과 이미지 SVGR props 사용하여 구현

--- src/constants/result/total.tsx
+++ src/constants/result/total.tsx
@@ -1,12 +1,12 @@
-import FirstSmallResultType from '@/assets/images/result/firstSmallResultType.svg';
-import FourthSmallResultType from '@/assets/images/result/forthSmallResultType.svg';
-import SecondSmallResultType from '@/assets/images/result/secondSmallResultType.svg';
-import ThirdSmallResultType from '@/assets/images/result/thirdSmallResultType.svg';
+import FirstResultType from '@/assets/images/result/firstResultType.svg';
+import ForthResultType from '@/assets/images/result/forthResultType.svg';
+import SecondResultType from '@/assets/images/result/secondResultType.svg';
+import ThirdResultType from '@/assets/images/result/thirdResultType.svg';
 
 export const RESULT_TYPE_LIST = [
   {
-    id: 1,
-    image: <FirstSmallResultType />,
+    id: 0,
+    image: <FirstResultType width={100} />,
     text: {
       firstLine: '내 결혼식날',
       secondLine: '나타나지 않을 원수',
@@ -15,8 +15,8 @@ export const RESULT_TYPE_LIST = [
     },
   },
   {
-    id: 2,
-    image: <SecondSmallResultType />,
+    id: 1,
+    image: <SecondResultType width={100} />,
     text: {
       firstLine: '내 결혼식날 자리에서',
       secondLine: '유튜브만 볼 남남',
@@ -25,8 +25,8 @@ export const RESULT_TYPE_LIST = [
     },
   },
   {
-    id: 3,
-    image: <ThirdSmallResultType />,
+    id: 2,
+    image: <ThirdResultType width={100} />,
     text: {
       firstLine: '내 결혼식날 단체사진',
       secondLine: '플래시를 켜 줄 지인',
@@ -35,8 +35,8 @@ export const RESULT_TYPE_LIST = [
     },
   },
   {
-    id: 4,
-    image: <FourthSmallResultType />,
+    id: 3,
+    image: <ForthResultType width={100} />,
     text: {
       firstLine: '내 결혼식날 내 부모님',
       secondLine: '뒷자리에 앉을 친구',
