9617d292704da14efcc3efba74202c6211ac40ab
sungjihyun <jhsung23@gmail.com>
2024-03-10 12:02:42 +0900

    :sparkles: 지난 테스트 조회 기능

--- src/app/mypage/_components/MyTest.tsx
+++ src/app/mypage/_components/MyTest.tsx
@@ -1,9 +1,25 @@
+'use client';
+
+import { ResultCard, ResultCardContainer } from '@/components/features/test/resultCard';
 import { EmptyTest } from '@/components/shared';
+import { useGetMyTest } from '@/hooks/test';
 
 const MyTest = () => {
+  const { data, status } = useGetMyTest();
+
   return (
     <>
-      <EmptyTest />
+      <ResultCardContainer>
+        {status === 'pending' ? (
+          <></>
+        ) : status === 'error' ? (
+          <></>
+        ) : data.length > 0 ? (
+          data.map((result) => <ResultCard key={result.id} result={result} />)
+        ) : (
+          <EmptyTest />
+        )}
+      </ResultCardContainer>
     </>
   );
 };
