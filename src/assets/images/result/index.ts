9740f5525f74d824dc83370678fc3f0bfc59a0d7
sungjihyun <jhsung23@gmail.com>
2024-03-10 11:39:18 +0900

    :heavy_plus_sign: 마이페이지 테스트 결과 조회용 svg 에셋

---
+++ src/assets/images/result/index.ts
@@ -0,0 +1,4 @@
+export { default as Result0 } from './result0-domonly.svg';
+export { default as Result100 } from './result100-domonly.svg';
+export { default as Result36 } from './result36-domonly.svg';
+export { default as Result70 } from './result70-domonly.svg';
