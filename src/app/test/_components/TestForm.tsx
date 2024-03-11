26e5e34763ade6a75b2f5d72462ec89b63aa1e7b
CodyMan0 <hys83751952@gmail.com>
2024-03-11 13:03:52 +0900

    :sparkles: 메인 페이지 슬롯 머신 hook 구현

--- src/app/test/_components/TestForm.tsx
+++ src/app/test/_components/TestForm.tsx
@@ -7,10 +7,12 @@ import MainLogo from '@/assets/images/mainLogo.svg';
 import { Button } from '@/components/common/button';
 import { Input } from '@/components/common/input';
 import { Header } from '@/components/layout/header';
+import { SLOT_AMOUNT_LIST } from '@/constants/test';
 import { PRE_QUESTIONS_LENGTH, QUESTIONS_ORDERS_LENGTH } from '@/constants/test/progress';
 import { QUESTIONS, QUESTIONS_ORDERS } from '@/constants/test/step';
 import { Typography } from '@/foundations/typography';
 import { useToast } from '@/hooks';
+import { useSlotMachine } from '@/hooks/useSlotMachine';
 import { Range } from '@/types/util';
 
 import TestLoading from './TestLoading';
@@ -22,6 +24,10 @@ const TestForm = () => {
   const toast = useToast();
   const [state, dispatch] = useReducer(reducer, initialState);
   const [step, setStep] = useState<StepProps>(0);
+  const { SlotMachine } = useSlotMachine({
+    textList: SLOT_AMOUNT_LIST,
+    duration: 10,
+  });
 
   const handleChangeStep = (index: StepProps) => {
     setStep((index + 2) as StepProps);
@@ -55,9 +61,7 @@ const TestForm = () => {
             <div className="flex flex-col items-center justify-center py-sm">
               <h1 className="text-[32px] font-semibold">축의금 얼마 내지?</h1>
               <div className="h-md w-6xl rounded-md border-2 border-primary-700 text-center">
-                <Typography type="heading1" className="text-primary-700">
-                  $ ??,???
-                </Typography>
+                <SlotMachine />
               </div>
               <Typography type="title4" className="pt-4xs text-gray-500">
                 상대와의 친밀도 알아보고 축의금 결정해보세요
--- src/constants/test/index.ts
+++ src/constants/test/index.ts
@@ -1,4 +1,5 @@
 export { QUESTIONS_ORDERS_LENGTH } from './progress';
 
+export { SLOT_AMOUNT_LIST } from './slot';
 export { QUESTIONS, QUESTIONS_ORDERS } from './step';
 
