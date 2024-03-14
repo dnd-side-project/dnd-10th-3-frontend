d9d70fa1abc01fd51cf697bed7ffc14523dc09ef
sungjihyun <jhsung23@gmail.com>
2024-03-14 16:27:24 +0900

    :heavy_minus_sign: VoteItem 관련 컴포넌트 파일 삭제

--- src/components/features/vote/voteItem/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as IconButton } from './IconButton';
-export { default as Img } from './Img';
-export { default as Input } from './Input';
-export { default as Progress } from './Progress';
-export { default as Text } from './Text';
