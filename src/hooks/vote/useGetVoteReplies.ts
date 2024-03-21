6c676f0077ac4b905944344a701b3df83acaf3d2
sungjihyun <jhsung23@gmail.com>
2024-03-21 13:11:35 +0900

    :recycle: 댓글 조회 suspense

--- src/hooks/vote/useGetVoteReplies.ts
+++ src/hooks/vote/useGetVoteReplies.ts
@@ -1,11 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 
 import { donworryApi } from '@/api';
 import { queryKey } from '@/api/queryKey';
 import { GetVoteRepliesRequest } from '@/api/vote/types';
 
 const useGetVoteReplies = ({ voteId }: GetVoteRepliesRequest) => {
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: queryKey.vote.reply(voteId),
     queryFn: () => donworryApi.vote.getVoteReplies({ voteId }),
   });
