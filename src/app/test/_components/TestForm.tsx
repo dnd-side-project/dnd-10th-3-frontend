b38a78770c7fd47294f7676f613240f975f30c94
이주영 <93697790+CodyMan0@users.noreply.github.com>
2024-03-29 08:42:33 +0900

    [FEAT] 메인 페이지 UI (#200)

--- src/app/test/_components/TestForm.tsx
+++ src/app/test/_components/TestForm.tsx
@@ -1,11 +1,9 @@
 'use client';
 
-import Image from 'next/image';
 import { useReducer, useState } from 'react';
 
 import { initialState, reducer } from '@/app/test/_helper/reducer';
-import LogoImage from '@/assets/images/logo.png';
-import { default as WorryImage } from '@/assets/images/test-worry.png';
+import MainLogo from '@/assets/images/mainLogo.svg';
 import { Button } from '@/components/common/button';
 import { Input } from '@/components/common/input';
 import { Typography } from '@/components/common/typography';
@@ -19,7 +17,6 @@ import TestLoading from './TestLoading';
 import TestQuestionTemplate from './TestQuestionTemplate';
 
 export type StepProps = Range<0, 12>;
-
 const TestForm = () => {
   const toast = useToast();
   const [state, dispatch] = useReducer(reducer, initialState);
@@ -43,6 +40,7 @@ const TestForm = () => {
   };
 
   return (
+    // 테스트 시작 랜딩 페이지 다음 PR SSR로 분리 예정
     <>
       {step === QUESTIONS_ORDERS.home && (
         <div className="relative flex size-full h-dvh flex-col items-center bg-mainGradient pb-2xs">
@@ -52,28 +50,39 @@ const TestForm = () => {
             <Header.IconLink href="/mypage" icon="mypage" />
           </Header>
 
-          <main className="flex size-full flex-col items-center px-2xs">
-            <div className="flex h-full flex-col items-center justify-evenly">
-              <div className="flex flex-col items-center gap-4xs">
-                <Image src={LogoImage} width={150} height={91} alt="logo" priority />
-                <Typography type="title1">축의금 결정이 너무 어려웠나요?</Typography>
+          <main className="flex size-full flex-col items-center">
+            <div className="flex flex-col items-center justify-center py-sm">
+              <h1 className="text-[32px] font-semibold">축의금 얼마 내지?</h1>
+              <div className="h-md  rounded-md border-2 border-primary-700 text-center">
+                <div className="flex items-center justify-between gap-2 px-4xs text-[24px] font-bold leading-[145%] text-primary-700">
+                  <span>₩</span>
+                  <p>??,???</p>
+                </div>
               </div>
-
-              <div className="flex flex-col items-center gap-xs">
-                <Image src={WorryImage} width={275} alt="logo" priority />
+              <Typography type="title4" className="pt-4xs text-gray-500">
+                상대와의 친밀도 알아보고 축의금 결정해보세요
+              </Typography>
+            </div>
+            <section className=" flex w-full flex-1 flex-col items-center">
+              <MainLogo width="100%" height="70%" />
+              <div className="w-[70%]">
                 <Input
                   type="fit"
                   placeholder=" 상대 이름을 입력해주세요"
-                  className="text-center"
+                  className="text-center placeholder:text-gray-1000"
+                  bgcolor="lightGray"
                   value={state.buddy}
                   onChange={(e) => dispatch({ type: 'setBuddyName', value: e.target.value })}
                   onKeyUp={handleOnKeyEnter}
                 />
               </div>
-            </div>
-            <Button width="full" onClick={handleTestFormInvalid}>
-              테스트하고 축의금 알아보기
-            </Button>
+            </section>
+
+            <footer className="w-full px-2xs">
+              <Button width="full" onClick={handleTestFormInvalid}>
+                테스트하고 축의금 알아보기
+              </Button>
+            </footer>
           </main>
         </div>
       )}
